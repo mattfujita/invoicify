590b9b5ca834f6410078b3fe691ba0f005372474
Matt Fujita <matthew.fujita@libertymutual.com>
2017-09-12 15:33:53 -0700

    added page routing

--- src/main/java/com/libertymutual/goforcode/invoicify/controllers/BillingRecordController.java
+++ src/main/java/com/libertymutual/goforcode/invoicify/controllers/BillingRecordController.java
@@ -1,10 +1,16 @@
 package com.libertymutual.goforcode.invoicify.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/billing-records")
+@RequestMapping("")
 public class BillingRecordController {
+	
+	@GetMapping("/billing-records")
+	public String billingRecords() {
+		return "billing-records/list";
+	}
 
 }
--- src/main/java/com/libertymutual/goforcode/invoicify/controllers/HomeController.java
+++ src/main/java/com/libertymutual/goforcode/invoicify/controllers/HomeController.java
@@ -1,10 +1,16 @@
 package com.libertymutual.goforcode.invoicify.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class HomeController {
+	
+	@GetMapping("")
+	public String defaultPage() {
+		return "default";
+	}
 
 }
